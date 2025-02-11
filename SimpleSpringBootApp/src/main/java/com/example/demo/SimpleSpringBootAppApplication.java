7f440b4b7bbad45b1a382c15f364865d1267f48b
Darshan D <dd656804@gmail.com>
2025-02-11 12:41:33 +0530

    Day9

---
+++ SimpleSpringBootApp/src/main/java/com/example/demo/SimpleSpringBootAppApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = ("com.eample.demo.controller"))
+public class SimpleSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringBootAppApplication.class, args);
+	}
+
+}
